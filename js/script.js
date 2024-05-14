892a13a72ef6e42034c3716b08bfac9a0669bfdd
prantikg96 <prantik.g1996@gmail.com>
2024-05-14 17:50:04 +0530

    create to-do list

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const addTaskBtn = () => {
+    const taskInput = document.getElementById('inputTask');
+    const taskInputValue = taskInput.value.trim();
+    
+    if(taskInputValue !== "")
+    {
+        const taskListUL = document.getElementById('taskList');
+        const li = document.createElement('li');
+        li.innerHTML = '<input type="checkbox"> ' + taskInputValue + ' <button onclick="delEachTask(this)">Delete This Task</button>';
+        taskListUL.appendChild(li);
+        taskInput.value = '';
+    }
+    else
+    {
+        alert("Please Add a Task !!");
+    }
+};
+
+const delEachTask = (delBtn) => {
+    // console.log(delBtn.parentNode);
+    const li = delBtn.parentNode;   // means we are taking li tag
+    li.parentNode.removeChild(li);   // means we are taking ul tag
+};
+
+const delCheckedBtn = () => {
+    const taskList = document.getElementById('taskList');
+    const checkBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    // console.log(checkBoxes); // returns as NodeList array
+
+    // to separate each items, we are using forEach loop here:
+    checkBoxes.forEach(checkBox => {
+        // console.log(checkBox.parentNode);    // li is checkBox's parent node
+        const li = checkBox.parentNode;
+        li.parentNode.removeChild(li);
+    })
+};
+
+
+
+
+// if we use trim() then it will remove/not consider the unnecessary spaces before and after a sentence.
+// eg:
+// console.log("    Hello All      "); // this will show spaces
+// console.log("    Hello All      ".trim()); // this will not show spaces
